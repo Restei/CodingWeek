5fc48a9ce21d85d8ff15b68ad4ac515c218e44ba
Clément Escude--Cotinat <clement.escude--cotinat@telecomnancy.eu>
2025-01-06 15:31:27 +0100

    build: permet de run avec build.gradle

--- app/src/main/java/grp04/jeu/Main.java
+++ app/src/main/java/grp04/jeu/Main.java
@@ -1,4 +1,31 @@
 package grp04.jeu;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Label vuePartie = new Label("hello");
+
+        //Partie partie = new Partie();
+        //GestionnairePartie gestionnairePartie = new GestionnairePartie(partie);
+        //VuePartie vuePartie = new VuePartie();
+
+        BorderPane root = new BorderPane();
+        root.setCenter(vuePartie);
+
+        Scene scene = new Scene(root, 1200, 800);
+        stage.setScene(scene);
+        stage.setTitle("LinguaCrypt : VuePartie");
+        stage.setResizable(false);
+        stage.show();
+    }
 }
