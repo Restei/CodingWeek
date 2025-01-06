e9e5752029e9c2631216f41222747753ebb41e08
Clément Escude--Cotinat <clement.escude--cotinat@telecomnancy.eu>
2025-01-06 14:10:32 +0100

    feat: implémente SujetObserve et Observateur

---
+++ app/src/main/java/grp04/jeu/modele/SujetObserve.java
@@ -0,0 +1,18 @@
+package grp04.jeu.modele;
+
+import grp04.jeu.vues.Observateur;
+import java.util.ArrayList;
+
+// Classe abstraite que les objets pouvant être observés doivent étendre.
+public abstract class SujetObserve {
+    private ArrayList<Observateur> observateurs;
+
+    public void NotifierObservateurs() {
+        for (Observateur obs : observateurs) {
+            obs.reagir();
+        }
+    }
+    public void ajouterObservateur(Observateur observateur) {
+        observateurs.add(observateur);
+    }
+}
