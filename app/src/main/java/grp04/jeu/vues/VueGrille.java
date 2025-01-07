61583726078f168e054de14c59801c83a016bdef
Clément Escude--Cotinat <clement.escude--cotinat@telecomnancy.eu>
2025-01-07 16:35:08 +0100

    format(VueGrille)

--- app/src/main/java/grp04/jeu/vues/VueGrille.java
+++ app/src/main/java/grp04/jeu/vues/VueGrille.java
@@ -1,45 +1,45 @@
 package grp04.jeu.vues;
+
 import grp04.jeu.modele.Grille;
 import grp04.jeu.modele.SujetObserve;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
-public class VueGrille extends GridPane implements Observateur{
+public class VueGrille extends GridPane implements Observateur {
     private Grille grille;
     private VueCarte[][] agent;
     private VueCarte[][] espion;
     private int index;
 
-    public VueGrille(Grille grille){
-           this.grille = grille;
-           //grille.ajouterObservateur(this);
-           this.index = 0;
-           this.agent = new VueCarte[grille.size()][grille.size()];
-           this.espion = new VueCarte[grille.size()][grille.size()];
-           for (int i =0;i<grille.size();i++) {
-               for (int j = 0; j < grille.size(); j++) {
-                   VueCarte carte = (new VueCarte(grille.getCarte(i, j),i,j));
-                   this.add(carte, i, j);
-                   agent[i][j]= carte;
-               }
-           }
-    }
-
-    public void reagir(){
-        this.getChildren().clear();
-        if (index==0){
-            index=1;
-            for (int i =0;i< agent.length;i++){
-                for (int j =0;j<agent.length;j++){
-                    this.add(espion[i][j],i,j);
-                }
+    public VueGrille(Grille grille) {
+        this.grille = grille;
+        //grille.ajouterObservateur(this);
+        this.index = 0;
+        this.agent = new VueCarte[grille.size()][grille.size()];
+        this.espion = new VueCarte[grille.size()][grille.size()];
+        for (int i = 0; i < grille.size(); i++) {
+            for (int j = 0; j < grille.size(); j++) {
+                VueCarte carte = (new VueCarte(grille.getCarte(i, j), i, j));
+                this.add(carte, i, j);
+                agent[i][j] = carte;
             }
         }
-        else{
-            index=0;
-            for (int i =0;i< agent.length;i++){
-                for (int j =0;j<agent.length;j++){
-                    this.add(agent[i][j],i,j);
+    }
+
+    public void reagir() {
+        this.getChildren().clear();
+        if (index == 0) {
+            index = 1;
+            for (int i = 0; i < agent.length; i++) {
+                for (int j = 0; j < agent.length; j++) {
+                    this.add(espion[i][j], i, j);
+                }
+            }
+        } else {
+            index = 0;
+            for (int i = 0; i < agent.length; i++) {
+                for (int j = 0; j < agent.length; j++) {
+                    this.add(agent[i][j], i, j);
                 }
             }
 
